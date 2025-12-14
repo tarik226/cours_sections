1108e480541be68ede240ff5b101e2e4a44accb0
tarikk <tarikelyatim2006@gmail.com>
2025-12-14 16:16:20 +0100

    le bas du page

---
+++ Desktop/LMS/footer.php
@@ -0,0 +1,6 @@
+</div> <!-- end container -->
+<footer style="background:#333; color:#fff; text-align:center; padding:10px; margin-top:20px;">
+    <p>&copy; <?= date('Y') ?> My LMS Platform</p>
+</footer>
+</body>
+</html>
